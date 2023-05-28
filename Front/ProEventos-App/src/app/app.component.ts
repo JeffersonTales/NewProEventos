1cf2b1485a42d932f6dcab635e6dbe9a7312d7b5
Jefferson Tales Canalli <jefferson.canalli@hotmail.com>
2023-05-28 12:41:44 -0300

    components

--- Front/ProEventos-App/src/app/app.component.ts
+++ Front/ProEventos-App/src/app/app.component.ts
@@ -4,23 +4,23 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  public forecasts?: WeatherForecast[];
+ // public forecasts?: WeatherForecast[];
 
   constructor(http: HttpClient) {
-    http.get<WeatherForecast[]>('/weatherforecast').subscribe(result => {
-      this.forecasts = result;
-    }, error => console.error(error));
+    //http.get<WeatherForecast[]>('/weatherforecast').subscribe(result => {
+    //  this.forecasts = result;
+    //}, error => console.error(error));
   }
 
   title = 'ProEventos-App';
 }
 
-interface WeatherForecast {
-  date: string;
-  temperatureC: number;
-  temperatureF: number;
-  summary: string;
-}
+//interface WeatherForecast {
+//  date: string;
+//  temperatureC: number;
+//  temperatureF: number;
+//  summary: string;
+//}
